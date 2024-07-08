e061ef81b425b22f7fb100f0ca5e2297d12d8984
michalcecko <ceckomichal@gmail.com>
2024-07-08 09:12:04 +0200

    Beta verzia table componentu

---
+++ theme/Models/Other/NameDay.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Theme\Models\Other;
+
+use Saurus\App\Modules\Wordpress\Models\Model;
+
+class NameDay extends Model
+{
+    protected $table = 'name_days';
+
+    protected $fillable = [
+        'name',
+        'day',
+        'month'
+    ];
+}
