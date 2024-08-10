a1b169a7adde913b1f1d3b6113722a932dfdb292
michalcecko <ceckomichal@gmail.com>
2024-08-10 09:57:31 +0200

    Changes

--- resources/views/parts/other/akcia.blade.php
+++ resources/views/parts/other/akcia.blade.php
@@ -1,15 +1,14 @@
-<div class="custom-dialog-wrapper" id="akciaDialog">
-
-    <div class="backdrop"></div>
-
-    <div class="custom-dialog">
-        <div class="custom-dialog--header">
-            <h5 class="custom-dialog--header--title">LPG novinka</h5>
-            <button type="button" class="close"></button>
-        </div>
-        <div class="custom-dialog--body">
-            <img src="{{ main()->assets()->static("images/akcie/august_2024.jpg") }}" alt="Akcie na august 2024">
+@if(!empty($imgSrc = get_field("akcia_letak", "options")))
+    <div class="custom-dialog-wrapper" id="akciaDialog">
+        <div class="backdrop"></div>
+        <div class="custom-dialog">
+            <div class="custom-dialog--header">
+                <h5 class="custom-dialog--header--title">Akcia</h5>
+                <button type="button" class="close"></button>
+            </div>
+            <div class="custom-dialog--body">
+                <img src="{{ $imgSrc }}" alt="Aktuálna akcia">
+            </div>
         </div>
     </div>
-
-</div>
+@endif
